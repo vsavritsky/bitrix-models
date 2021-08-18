34661bf572b3549c582ca8c751c2e169a26a9240
Vladimir Savritsky <vladimir.savritsky@gmail.com>
2021-08-18 16:23:09 +0300

    add code element code

--- src/Entity/BaseModel.php
+++ src/Entity/BaseModel.php
@@ -3,6 +3,8 @@
 namespace BitrixModels\Entity;
 
 use LogicException;
+use CPHPCache;
+use \Bitrix\Iblock\IblockTable;
 
 class BaseModel
 {
@@ -26,6 +28,27 @@ class BaseModel
     public static function iblockId()
     {
         $id = static::IBLOCK_ID;
+        $code = static::IBLOCK_CODE;
+
+        if (!$id) {
+            $obCache = new CPHPCache();
+            $cacheTime = 360000;
+            $cacheId = md5($code);
+            $cachePath = DIRECTORY_SEPARATOR . SITE_ID . DIRECTORY_SEPARATOR;
+            if ($obCache->InitCache($cacheTime, $cacheId, $cachePath)) {
+                $vars = $obCache->GetVars();
+                $result = $vars['result'];
+            } elseif ($obCache->StartDataCache()) {
+                $arIblock = \Bitrix\Iblock\IblockTable::getList(array(
+                    'filter' => array('CODE' => $code),
+                ))->fetch();
+                $result = $arIblock;
+                $obCache->EndDataCache(['result' => $result]);
+            }
+
+            $id = $result['ID'];
+        }
+
         if (!$id) {
             throw new LogicException('You must set IBLOCK_ID constant inside a model or override iblockId() method');
         }
@@ -51,13 +74,13 @@ class BaseModel
         return null;
     }
 
-    public function mapData($data) : self
+    public function mapData($data): self
     {
 
         return $this;
     }
 
-    public function toArray() :array
+    public function toArray(): array
     {
 
         return [];
